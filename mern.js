78d2ff59361ea60ce518c30cd89408f98ba875b8
kishoreprasathece <kishoreprasathece@gmail.com>
2024-08-24 17:19:54 +0530

    upd1

--- mern.js
+++ mern.js
@@ -1,13 +1,123 @@
-function firstname(){
-    console.log("kishore")
-    return function secondname(){
-    console.log("prasath")
+1
+//alert("invalid email")
+
+2
+
+//let name= prompt("enter the name");
+//console.log(name);
+
+3
+
+//let a=Number(prompt("enter the value"));
+//console.log(a*a);
+
+4
+//let a=999,b=78;
+//console.log(a+b);
+//console.log(a-b);
+//console.log(a*b);
+//console.log(a/b);
+// console.log(a**b);
+// console.log(a);
+
+5 
+// let a=true , b=false;
+// console.log(a && b);
+
+// let c=true , d=true;
+// console.log(c && d);
+
+// console.log(!c);
+// console.log(!d);
+// console.log(c || d);
+
+// let firstname="kishore"
+// let secondname="prasath"
+
+// console.log(firstname=="kishore" && secondname=="prasath");
+// console.log(firstname=="mohan" && secondname=="prasath");
+// console.log(!firstname=="prasath");
+// console.log(firstname=='kishore' || secondname=="kishore");
+// console.log(firstname=="kishore");
+// console.log(firstname=="prasath");
+
+6 
+// let obj={name:"kis",age:29}
+// console.log(obj);
+// console.log(obj.name);
+
+// let arr=["kishore","prasath"]
+// console.log(arr[1]);
+
+// let age=[9,8,9,7]
+// console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function firstname(){
+//     console.log("kishore")
+//     return function secondname(){
+//     console.log("prasath")
     
-    }
+//     }
     
-}
-//firstname()
-let res=firstname()
-res()
+// }
+// //firstname()
+// let res=firstname()
+// res()
 
 alert("invalid email")
--- readme.md
+++ readme.md
@@ -1,2 +1,17 @@
-TASK 1
- ABOUT HIGHER ORDER FUNCTION
+ 1
+alert is used to give pop up to user
+
+2
+
+prompt used to get info  from user and has ok and cancel
+
+3
+ 
+ to get values using prompt
+
+ 4
+different type of operators
+
+5
+VAR SUPPORTS HOISTING BUT LET DOESNT SUPPORT HOISTING
+VAR ALLOW REASSIGN WHERE LET DOESNT

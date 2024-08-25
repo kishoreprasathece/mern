2c485fcc3c91205c4e9a1fa53f83c9ba6b25437e
kishoreprasathece <kishoreprasathece@gmail.com>
2024-08-25 20:05:02 +0530

    update of task day 2

--- mern.js
+++ mern.js
@@ -1,17 +1,17 @@
-1
+//1
 //alert("invalid email")
 
-2
+//2
 
 //let name= prompt("enter the name");
 //console.log(name);
 
-3
+//3
 
 //let a=Number(prompt("enter the value"));
 //console.log(a*a);
 
-4
+//4
 //let a=999,b=78;
 //console.log(a+b);
 //console.log(a-b);
@@ -20,7 +20,7 @@
 // console.log(a**b);
 // console.log(a);
 
-5 
+//5 
 // let a=true , b=false;
 // console.log(a && b);
 
@@ -41,7 +41,7 @@
 // console.log(firstname=="kishore");
 // console.log(firstname=="prasath");
 
-6 
+//6 
 // let obj={name:"kis",age:29}
 // console.log(obj);
 // console.log(obj.name);
@@ -52,72 +52,223 @@
 // let age=[9,8,9,7]
 // console.log(age);
 
+// let obj={name:"kishore",password:55}
+// console.log(obj);
+
+//let age=['q','w','e','t','y']
+//console.log(age[4]);
 
 
+//7
 
+// let a=999,b=898;
+// function add() {
+// console.log(a*b);
 
+// }
+// add()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function firstname(){
-//     console.log("kishore")
-//     return function secondname(){
-//     console.log("prasath")
+// const entername= "kishore" ;
+// function name(){
+//     console.log(entername);
     
+// }
+// name()
+
+
+
+// const name= arrfunc=()=>{
+//     console.log("kishore");
+    
+// }
+// arrfunc()
+
+// let a= name=()=>{
+//     console.log("prasath");
+    
+// }
+// name()
+
+
+
+// const user = arrfunc=()=>{
+//     console.log("kishore");
+//     console.log("prasath");
+    
+    
+//     }
+//     arrfunc()
+
+// function name(){
+//     console.log("prasath");
+    
+// }
+// name()
+
+// const name=()=>{
+//     console.log("tom ellis");
+    
+// }
+
+
+
+// const res= function(a,b){
+//     console.log(a,b);
+//     }
+// res("laptop","mobile")
+
+// (function(){
+//     console.log("tomellis");
+    
+// })();
+
+// (function(){
+//     let a="kishore"
+//     console.log(a);
+    
+// })();
+
+
+// (
+//     function(){
+//         let a=99999,b=222;
+//         console.log(a*b);
+        
+//     }
+// )
+
+//9
+
+// function setalaram(){
+//     return 10;
+// }
+// let time = setalaram()
+// console.log(time);
+
+// function login(){
+//     return 'kishore';
+// }
+//     let nam= login()
+//     console.log(nam);
+    
+
+
+
+
+// function naveen(totalmark){
+// return 988;
+// }
+// let naveenresult = naveen();
+// console.log(naveenresult);
+
+
+// function fathername(){
+//     console.log("raja ravi varman");
+//     return function sonname(){
+//         console.log("vishnu");
+//     };
+    
+    
+// }
+// //fathername();
+// const sonname= fathername();
+// sonname();
+
+
+// function setalaram(){
+//     console.log(10);
+//     function snooze(){
+// console.log(11);
+
 //     }
     
 // }
-// //firstname()
-// let res=firstname()
-// res()
+// //setalaram();
+// let snooze= setalaram();
+// snooze();
 
-alert("invalid email")
+// function gender(a){
+//     return "kishore";
+// }
+// const name= gender();
+//  console.log(name);
+ 
+// function model(iphone){
+//     return "iphone11";
+// }
+// const avail= model();
+// console.log(avail);
+
+// 15 higher order function
+
+
+
+// function model(){
+//     console.log("iphone");
+//     return function num(){
+//         console.log(15);
+        
+//     }
+// }
+// //model():
+// let modelnum= model();
+// modelnum();
+
+
+//16- call back
+
+// function dis(){
+//     console.log("available model");
+    
+// }
+
+// function model(callback){
+//     console.log("iphone");
+// callback();
+//     return function num(){
+//         console.log(15);
+        
+//     }
+// }
+// //model():
+// let modelnum= model(dis);
+// modelnum();
+
+
+// //17- ternary
+
+// let price= 1000;
+// const res = price > 1000 ? "greater than 1000 " : "less than 1000"; 
+// console.log(res);
+
+// let availableproduct = "macbook";
+// const res = availableproduct == "hp" ? "yes" : "no";
+// console.log(res);
+
+// let password= 006;
+// const res= password== 006 ? "yes" : "no";
+// console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
